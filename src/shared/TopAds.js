588c9f8f2b9f86ffcc69d4f31d7064731079c827
NJAbuin <nicolas@abuin.com.ar>
2020-01-12 14:24:58 -0500

    hr, search background commented

--- src/shared/TopAds.js
+++ src/shared/TopAds.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function TopAds() {
   return (
     <div className="topBanner">
+      <hr />
       <div className="topAdGrayBox"></div>
       <div className="topAdBox"></div>
       <div className="topAdGrayBox"></div>
